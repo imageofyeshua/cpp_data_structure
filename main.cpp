c9d643be46ebb5e5e9c29b3c9cc9f5240f435546
Daniel Park <intheimageofyeshua@gmail.com>
2025-03-10 23:38:15 +0900

    BitShift

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include "control.h"
 #include "inputoutput.h"
-#include <bitset>
+#include "operator.h"
 
 using namespace std;
 
-const long double km_per_mile = 1.690344L;
-long double operator"" _km(long double val) { return val; }
-long double operator"" _mi(long double val) { return val * km_per_mile; }
+/*const long double km_per_mile = 1.690344L;*/
+/*long double operator"" _km(long double val) { return val; }*/
+/*long double operator"" _mi(long double val) { return val * km_per_mile; }*/
 
 int main() {
-    Control con;
+    Operator op;
+    op.BitShift();
+
+    /*Control con;*/
     /*con.StudentReport();*/
     /*con.NumberGuessing();*/
     /*con.TelephoneDigits();*/
@@ -26,27 +29,27 @@ int main() {
     /*io.TicketSales();*/
     /*io.StudentReport();*/
 
-    unsigned int value = 0x0000000;
-
-    value = ~value;
-    cout << hex << value << endl;
-
-    long double distance_1 = 1.0_km;
-    long double distance_2 = 1.0_mi;
-
-    cout << distance_1 + distance_2 << " km" << endl;
-
-    int a = 13;
-    int b = 27;
-    int c = a & b;
-    int d = a | b;
-    int e = a ^ b;
-
-    cout << "a = " << bitset<8>(a) << " : " << a << endl;
-    cout << "b = " << bitset<8>(b) << " : " << b << endl;
-    cout << "c = " << bitset<8>(c) << " : " << c << endl;
-    cout << "d = " << bitset<8>(d) << " : " << d << endl;
-    cout << "e = " << bitset<8>(e) << " : " << e << endl;
+    /*unsigned int value = 0x0000000;*/
+    /**/
+    /*value = ~value;*/
+    /*cout << hex << value << endl;*/
+    /**/
+    /*long double distance_1 = 1.0_km;*/
+    /*long double distance_2 = 1.0_mi;*/
+    /**/
+    /*cout << distance_1 + distance_2 << " km" << endl;*/
+    /**/
+    /*int a = 13;*/
+    /*int b = 27;*/
+    /*int c = a & b;*/
+    /*int d = a | b;*/
+    /*int e = a ^ b;*/
+    /**/
+    /*cout << "a = " << bitset<8>(a) << " : " << a << endl;*/
+    /*cout << "b = " << bitset<8>(b) << " : " << b << endl;*/
+    /*cout << "c = " << bitset<8>(c) << " : " << c << endl;*/
+    /*cout << "d = " << bitset<8>(d) << " : " << d << endl;*/
+    /*cout << "e = " << bitset<8>(e) << " : " << e << endl;*/
 
     return 0;
 }
---
+++ operator.h
@@ -0,0 +1,11 @@
+#include <bitset>
+#include <iostream>
+
+using namespace std;
+
+class Operator {
+    public:
+        int BitShift();
+
+    private:
+};
