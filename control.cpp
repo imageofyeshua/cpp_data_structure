304a6dc7fb4f5d67ea7f23f5750d14ea00e0df70
Daniel Park <intheimageofyeshua@gmail.com>
2025-03-10 13:25:54 +0900

    Billing Control Example

--- control.cpp
+++ control.cpp
@@ -11,8 +11,8 @@ void Control::Billing() {
     // Business Customers
     const double BUS_BILL_PROC_FEES = 15.00;
     const double BUS_BASIC_SERV_COST = 15.00;
-    const double BUS_BASIC_CONN_CONT = 5.00;
-    const double BUS_CONST_PREM_CHANNEL = 50.00;
+    const double BUS_BASIC_CONN_COST = 5.00;
+    const double BUS_COST_PREM_CHANNEL = 50.00;
 
     int accountNumber;
     char customerType;
@@ -23,4 +23,53 @@ void Control::Billing() {
     cout << fixed << showpoint << setprecision(2);
 
     cout << "This program computes a cable bill." << endl;
+
+    cout << "Enter accont number (an integer): ";
+    cin >> accountNumber;
+    cout << endl;
+
+    cout << "Enter customer type: R or r (Residential), "
+         << "B or b (Business): ";
+    cin >> customerType;
+    cout << endl;
+
+    switch (customerType) {
+    case 'r':
+    case 'R':
+        cout << "Enter the number of premium channels: ";
+        cin >> numOfPremChannels;
+        cout << endl;
+
+        amountDue = RES_BILL_PROC_FEES + RES_BASIC_SERV_COST +
+                    numOfPremChannels * RES_COST_PREM_CHANNEL;
+
+        cout << "Account number: " << accountNumber << endl;
+        cout << "Amount due: $" << amountDue << endl;
+        break;
+    case 'b':
+    case 'B':
+        cout << "Enter the number of basic service connections: ";
+        cin >> numOfBasicServConn;
+        cout << endl;
+
+        cout << "Enter the number of premium channels: ";
+        cin >> numOfPremChannels;
+        cout << endl;
+
+        if (numOfBasicServConn <= 10) {
+            amountDue = BUS_BILL_PROC_FEES + BUS_BASIC_SERV_COST +
+                        numOfPremChannels * BUS_COST_PREM_CHANNEL;
+        } else {
+            amountDue = BUS_BILL_PROC_FEES +
+                        (numOfBasicServConn - 10) * BUS_BASIC_CONN_COST +
+                        numOfPremChannels * BUS_COST_PREM_CHANNEL;
+        }
+
+        cout << "Account number: " << accountNumber << endl;
+        cout << "Amount due: $" << amountDue << endl;
+        break;
+
+    default:
+        cout << "Invalid customer type." << endl;
+    }
 }
